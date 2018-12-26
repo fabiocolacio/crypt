331615a61118f02ff55c4a7b81b137bb6131d5e0
Fabio Colacio <fabio.colacio.dev@gmail.com>
2018-12-26 10:53:14 -0800

    Add decrypt mode

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import(
     "flag"
     "fmt"
+    "errors"
     "io/ioutil"
     "crypto/aes"
     "crypto/cipher"
@@ -41,7 +42,14 @@ func main() {
         return
     }
 
-    err := encrypt(file, pass)
+    var err error
+
+    if decryptFlag {
+        err = decrypt(file, pass)
+    } else {
+        err = encrypt(file, pass)
+    }
+
     if err != nil {
         fmt.Println(err)
     }
@@ -94,6 +102,48 @@ func encrypt(infile, password string) error {
     tag := mac.Sum(nil)
     copy(macTag, tag)
 
+    cbc = cipher.NewCBCEncrypter(block, iv)
+    cbc.CryptBlocks(hmacKey, hmacKey)
+
     return ioutil.WriteFile(infile + ".gobbledygook", buf, 0666)
+}
+
+func decrypt(infile, password string) error {
+    buf, err := ioutil.ReadFile(infile)
+    if err != nil {
+        return err
+    }
+
+    iv := buf[:aes.BlockSize]
+    hmacKey := buf[aes.BlockSize:aes.BlockSize + HMAC_KEYSIZE]
+    macTag := buf[aes.BlockSize + HMAC_KEYSIZE:aes.BlockSize + HMAC_KEYSIZE + MAC_TAGSIZE]
+    cipherText := buf[aes.BlockSize + HMAC_KEYSIZE + MAC_TAGSIZE:]
+
+    aesKey := pbkdf2.Key([]byte(password), nil, KDF_ITERS, AES_KEYSIZE, sha256.New)
+    block, err := aes.NewCipher(aesKey)
+    if err != nil {
+        return err
+    }
+
+    cbc := cipher.NewCBCDecrypter(block, iv)
+    cbc.CryptBlocks(hmacKey, hmacKey)
+
+    mac := hmac.New(sha256.New, hmacKey)
+    mac.Write(iv)
+    mac.Write(cipherText)
+    expectedMac := mac.Sum(nil)
+    if !hmac.Equal(macTag, expectedMac) {
+        return errors.New("Expected MAC did not match supplied MAC.")
+    }
+
+    cbc = cipher.NewCBCDecrypter(block, iv)
+    cbc.CryptBlocks(cipherText, cipherText)
+
+    clearText, err := PKCS7Unpad(cipherText, aes.BlockSize)
+    if err != nil {
+        return err
+    }
+
+    return ioutil.WriteFile(infile + ".txt", clearText, 0666)
 }
 
